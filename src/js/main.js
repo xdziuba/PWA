de389e2e464b98569cadf7161f6a9412833fdecd
xdziuba <xdziuba@gmail.com>
2026-04-15 20:39:06 +0200

    Add base functionality

---
+++ src/js/main.js
@@ -0,0 +1,50 @@
+let currentPos;
+let photo;
+let map;
+let marker;
+let preview = document.getElementById("photoPreview");
+
+
+document.getElementById("photoCapture").addEventListener("change", (e) => {
+    photo = e.target.files[0];
+    if (photo) {
+        const imageURL = URL.createObjectURL(photo);
+        preview.src = imageURL;
+        preview.style.display = "block";
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            currentPos = position;
+            // alert(`Lat: ${currentPos.coords.latitude}, Lng: ${currentPos.coords.longitude}`);
+
+            map = L.map('map').setView(
+                [currentPos.coords.latitude, currentPos.coords.longitude], 13
+            );
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+                .addTo(map);
+
+            marker = L.marker([
+                currentPos.coords.latitude,
+                currentPos.coords.longitude
+            ]).addTo(map);
+        },
+        function(error) {
+            alert("Błąd pobierania lokalizacji: " + error.message);
+        }
+    );
+});
+
+
+function shareData() {
+    if (navigator.share) {
+        navigator.share({
+            title: 'Zgłoszenie zdarzenia',
+            text: 'Lokalizacja: ${currentPos.coords.latitude}, ${currentPos.coords.longitude}',
+            files: [photo]
+        });
+    } else {
+        alert("Brak wsparcia dla Web Share API");
+    }
+}
